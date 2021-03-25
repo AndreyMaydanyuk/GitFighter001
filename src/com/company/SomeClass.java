8feefd7c754db96d3158a19ebda6e1c3968504eb
Andrey Maydanyuk <andrey.maydanyuk@ab-soft.net>
2021-03-25 22:54:27 +0200

    greeting added

--- src/com/company/SomeClass.java
+++ src/com/company/SomeClass.java
@@ -3,5 +3,6 @@ package com.company;
 public class SomeClass {
     public static void main(String[] args) {
         System.out.println("Hello new class");
+        System.out.println("line of greeting");
     }
 }
