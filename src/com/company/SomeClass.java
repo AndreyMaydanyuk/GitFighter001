aedbef869337932c24c31366130338545a21694a
Andrey Maydanyuk <andrey.maydanyuk@ab-soft.net>
2021-03-25 22:52:09 +0200

    starting an experiment

--- src/com/company/SomeClass.java
+++ src/com/company/SomeClass.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class SomeClass {
     public static void main(String[] args) {
-        System.out.println("Hello new class");
+        System.out.println("Hello , world");
+        System.out.println("what is your name?");
     }
 }
