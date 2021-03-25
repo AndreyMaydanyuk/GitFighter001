9965d80054154737447aa16d9beae03af22ad125
Andrey Maydanyuk <andrey.maydanyuk@ab-soft.net>
2021-03-25 23:11:38 +0200

    fixed line8

--- src/com/company/SomeClass.java
+++ src/com/company/SomeClass.java
@@ -5,6 +5,6 @@ public class SomeClass {
         System.out.println("Hello , world");
         System.out.println("what is your name?");
         System.out.println("some new code");
-        System.out.println("line of greeting");
+        System.out.println("line of greeting!");
     }
 }
