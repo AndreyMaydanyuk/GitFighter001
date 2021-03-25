60cf6da4749de41c0fa04396b6e02b7370ca316f
Andrey Maydanyuk <andrey.maydanyuk@ab-soft.net>
2021-03-25 23:01:52 +0200

    fixed line7

--- src/com/company/SomeClass.java
+++ src/com/company/SomeClass.java
@@ -4,5 +4,6 @@ public class SomeClass {
     public static void main(String[] args) {
         System.out.println("Hello , world");
         System.out.println("what is your name?");
+        System.out.println("some new code");
     }
 }
