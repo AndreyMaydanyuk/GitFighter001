42a078fbb00127ba604b3071b60c1dc30a425c69
Andrey <Andrey@test.com>
2021-03-26 08:24:18 +0200

    add line3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello Git!!!");
         System.out.println("line1");
+        System.out.println("line number 3");
     }
 }
